5d8cdfb7760eaa44de64afcebfd93f467bcd695c
Rohith thammaiah <thammaiahrohith@gmail.com>
2019-06-30 11:12:58 +0530

    removed initial progress

--- app/src/main/java/com/rohith/progressselector/MainActivity.kt
+++ app/src/main/java/com/rohith/progressselector/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.rohith.progressselector
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+
+import androidx.appcompat.app.AppCompatActivity
+
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,28 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var i =0
-       /* var disposable = Observable.interval(
-            0,500, TimeUnit.MILLISECONDS)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                customProgressBar?.setProgress(i++)
-            }
-
-        customProgressBar?.listeners = object : IndeterminateProgressBar.ProgressBarListeners{
-            override fun onProgressStart(view: View?) {
-                Log.e("ProgressBar","Start")
-            }
-
-            override fun onProgressChanged(view: View?, value: Int) {
-                Log.e("ProgressBar","Running $value")
-            }
-
-            override fun onProgressCompleted(view: View?) {
-                Log.e("ProgressBar","Completed")
-            }
-
-        }*/
     }
 }
