a8f82712e1c9041919d4931b9d8f2fd64eaaa090
rohiththammaiah <rohit@iciindia.in>
2019-12-22 23:14:00 +0530

    refactored from IndeterminateProgressBar to DrawableProgressSlider

--- app/src/main/java/com/rohith/progressselector/MainActivity.kt
+++ app/src/main/java/com/rohith/progressselector/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.rohith.progressselector
 
 import android.os.Bundle
+import android.view.View
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        customProgressBar.setDrawable(R.drawable.ic_baseline_star_24)
+
+        customProgressBar.listeners = object : DrawableProgressSlider.ProgressBarListeners {
+            override fun onProgressStart(view: View?) = Unit
+            override fun onProgressChanged(view: View?, value: Float) = Unit
+            override fun onProgressCompleted(view: View?) = Unit
+        }
 
     }
 }
